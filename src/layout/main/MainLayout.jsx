955f5c1866d2935395bfc74c65ce7d7a54badd7f
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-14 14:44:56 +0300

    feat: add Form component with styling and functionality to MainLayout

--- src/layout/main/MainLayout.jsx
+++ src/layout/main/MainLayout.jsx
@@ -1,7 +1,12 @@
+import Form from "../form/Form.jsx";
 import "./MainLayout.css";
 
 function MainLayout() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <Form />
+    </div>
+  );
 }
 
 export default MainLayout;
