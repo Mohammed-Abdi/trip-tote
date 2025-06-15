36c10260d4af8e2e44d5fbda75d2134db89d4a1e
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-15 16:39:24 +0300

    feat: implement CheckList component with item mapping

---
+++ src/layout/checklist/CheckList.jsx
@@ -0,0 +1,15 @@
+import "./CheckList.css";
+import items from "../../data/items.js";
+import Item from "../../components/item/Item.jsx";
+
+function CheckList() {
+  return (
+    <div className="checklist">
+      {items.map((item) => {
+        return <Item name={item.name} quantity={item.quantity} />;
+      })}
+    </div>
+  );
+}
+
+export default CheckList;
