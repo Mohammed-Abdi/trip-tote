c6a52d57c2bc5e38a1c8cd52c1168b0f71b6399e
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-15 16:47:09 +0300

    feat: pass items and setItems to MainLayout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Header from "./layout/header/Header.jsx";
 import MainLayout from "./layout/main/MainLayout.jsx";
 function App() {
+  const [items, setItems] = useState([]);
   return (
     <>
       <Header />
-      <MainLayout />
+      <MainLayout items={items} setItems={setItems} />
     </>
   );
 }
