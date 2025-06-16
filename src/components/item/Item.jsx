8a294d31d39d38f70370e76fbfbf39855915de94
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-16 15:23:38 +0300

    fix: add deleteIcon class to delete icon in Item component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -24,7 +24,7 @@ function Item({ name, quantity, isPacked, setItems, id }) {
       <span className="item-number">{quantity}</span>
       <span className={`item-name ${isPacked ? "packed" : ""}`}>{name}</span>
       <div className="delete" onClick={deleteItem}>
-        <Icon icon="streamline-ultimate:bin-1-bold" />
+        <Icon icon="streamline-ultimate:bin-1-bold" className="deleteIcon" />
       </div>
     </div>
   );
