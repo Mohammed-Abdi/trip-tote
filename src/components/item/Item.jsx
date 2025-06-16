91ba63543d73fdfe4d60d9648432f6075204405d
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-16 15:33:12 +0300

    fix: update class name from item-name to item-info in Item component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -21,8 +21,10 @@ function Item({ name, quantity, isPacked, setItems, id }) {
   return (
     <div className="item">
       <input type="checkbox" className="checkbox" onClick={packItUp} />
-      <span className="item-number">{quantity}</span>
-      <span className={`item-name ${isPacked ? "packed" : ""}`}>{name}</span>
+      <span className="item-number"></span>
+      <span
+        className={`item-info ${isPacked ? "packed" : ""}`}
+      >{`${quantity} - ${name}`}</span>
       <div className="delete" onClick={deleteItem}>
         <Icon icon="streamline-ultimate:bin-1-bold" className="deleteIcon" />
       </div>
