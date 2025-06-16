fc89adb6d83f7f01232d233c70624e1aa8d64372
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-16 17:19:33 +0300

    fix: display total packed items in checklist component

--- src/layout/checklist/CheckList.jsx
+++ src/layout/checklist/CheckList.jsx
@@ -2,8 +2,15 @@ import "./CheckList.css";
 import Item from "../../components/item/Item.jsx";
 
 function CheckList({ items, setItems }) {
+  const numberOfPackedItems = items.filter(
+    (item) => item.isPacked === true
+  ).length;
+  const totalNumberOfItems = items.length;
   return (
     <div className="checklist">
+      <div className="total-items">{`${numberOfPackedItems}/${totalNumberOfItems}`}</div>
+
+      {console.log(`${numberOfPackedItems}/${items.length}`)}
       {items.map((item) => {
         return (
           <Item
