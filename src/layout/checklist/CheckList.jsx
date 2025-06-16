e01b0890d0473f3cda876c3f413fc436fa1e41f0
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-16 17:39:36 +0300

    fix: refactor checklist component layout and improve delete functionality

--- src/layout/checklist/CheckList.jsx
+++ src/layout/checklist/CheckList.jsx
@@ -6,23 +6,39 @@ function CheckList({ items, setItems }) {
     (item) => item.isPacked === true
   ).length;
   const totalNumberOfItems = items.length;
+  function deleteAll() {
+    setItems([]);
+  }
   return (
     <div className="checklist">
-      <div className="total-items">{`${numberOfPackedItems}/${totalNumberOfItems}`}</div>
+      {totalNumberOfItems > 0 ? (
+        <div className="total-items">{`${numberOfPackedItems}/${totalNumberOfItems}`}</div>
+      ) : (
+        ""
+      )}
 
-      {console.log(`${numberOfPackedItems}/${items.length}`)}
-      {items.map((item) => {
-        return (
-          <Item
-            key={item.id}
-            name={item.item}
-            quantity={item.quantity}
-            isPacked={item.isPacked}
-            setItems={setItems}
-            id={item.id}
-          />
-        );
-      })}
+      <div className="checklist-wrapper">
+        {console.log(`${numberOfPackedItems}/${items.length}`)}
+        {items.map((item) => {
+          return (
+            <Item
+              key={item.id}
+              name={item.item}
+              quantity={item.quantity}
+              isPacked={item.isPacked}
+              setItems={setItems}
+              id={item.id}
+            />
+          );
+        })}
+      </div>
+      {totalNumberOfItems > 0 ? (
+        <button className="delete-all" onClick={deleteAll}>
+          Delete All
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
