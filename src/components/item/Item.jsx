1322d215246e91f5beeff3ae97cd53856d131e7d
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-16 14:23:04 +0300

    fix: add conditional class for packed state in Item component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -6,7 +6,7 @@ function Item({ name, quantity, isPacked }) {
     <div className="item">
       <input type="checkbox" className="checkbox" />
       <span className="item-number">{quantity}</span>
-      <span className="item-name">{name}</span>
+      <span className={`item-name ${isPacked ? "packed" : ""}`}>{name}</span>
       <div className="delete">
         <Icon icon="streamline-ultimate:bin-1-bold" />
       </div>
