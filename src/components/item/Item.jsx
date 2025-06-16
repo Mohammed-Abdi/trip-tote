6219d43b78c8785efca8516d83c129a83402ac86
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-16 14:52:40 +0300

    fix: refactor packItUp and deleteItem functions to use index for state updates

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,16 +1,29 @@
 import "./Item.css";
 import { Icon } from "@iconify/react";
 
-function Item({ name, quantity, isPacked, setItems }) {
+function Item({ name, quantity, isPacked, setItems, index }) {
   function packItUp() {
-    setItems((items) => [...items, { ...items, isPacked: !isPacked }]);
+    setItems((prevItems) =>
+      prevItems.map((item, i) => {
+        return index === i ? { ...item, isPacked: !item.isPacked } : item;
+      })
+    );
   }
+
+  function deleteItem() {
+    setItems((prevItems) =>
+      prevItems.filter((item, i) => {
+        return index !== i;
+      })
+    );
+  }
+
   return (
     <div className="item">
       <input type="checkbox" className="checkbox" onClick={packItUp} />
       <span className="item-number">{quantity}</span>
       <span className={`item-name ${isPacked ? "packed" : ""}`}>{name}</span>
-      <div className="delete">
+      <div className="delete" onClick={deleteItem}>
         <Icon icon="streamline-ultimate:bin-1-bold" />
       </div>
     </div>
